1ba0b016041e469fe4b0b96d13c7e47c97b38347
xergon <christoffer.isenberg@gmail.com>
2025-05-13 10:27:05 +0200

    Added Non-generic ServiceResult

--- FribergHomeAPI/Results/ServiceResult.cs
+++ FribergHomeAPI/Results/ServiceResult.cs
@@ -14,4 +14,16 @@
         public static ServiceResult<T> Failure(string error) => new() { Success = false, Errors = [new ServiceResultError { Code = "", Description = error}] };
         public static ServiceResult<T> Failure(ServiceResultError error) => new() { Success = false, Errors = [error] };
     }
+    public class ServiceResult
+    {
+        public bool Success { get; init; }
+        public IEnumerable<ServiceResultError> Errors { get; init; } = Enumerable.Empty<ServiceResultError>();
+
+        private ServiceResult() { }
+
+        public static ServiceResult SuccessResult() => new() { Success = true };
+        public static ServiceResult Failure(IEnumerable<ServiceResultError> errors) => new() { Success = false, Errors = errors };
+        public static ServiceResult Failure(string error) => new() { Success = false, Errors = [new ServiceResultError { Code = "", Description = error }] };
+        public static ServiceResult Failure(ServiceResultError error) => new() { Success = false, Errors = [error] };
+    }
 }
